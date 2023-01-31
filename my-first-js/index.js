09efe5605fc9f8365a7525280ee71af79b6201a5
admin <you@example.com>
2023-01-31 10:53:02 +0100

    boiler plate

---
+++ my-first-js/index.js
@@ -0,0 +1,55 @@
+// my first js code
+console.log('hello world');
+
+/* let name = 'Vonn';
+console.log (name);
+
+//cannot be a reserved keyword
+//should be meaningful word
+//cannot start with a number
+//cannot contain a space or hypen (-)
+//are case-sensitive
+
+let firstName = 'VonnRyann';
+let lastName = 'Cruz';
+let interestRate = '0.3'
+interestRate = 1;
+console.log(interestRate)
+
+let name = 'Vonn';//string literal
+let age = 30;
+let isAprroved = false;
+let firstName = undefined;
+let lastName = null; */
+
+
+/* let person = {
+    name: 'VonnRyann',
+    age: 30
+};
+//this is the dot notation
+person.name = 'Vonn';
+//Bracket notation
+let selection = 'name'
+person ['name'] = 'Mary'
+console.log (person.name); */
+
+/* 
+let selectedColors = ['red', 'blue', 'black', 'orange'];
+selectedColors[0] = 5;
+console.log(selectedColors.length); */
+
+//performing a task
+function greet(name, lastName) {
+    console.log('hello ' + name + ' ' + lastName);
+}
+//calculating the value
+function square (number) {
+    return number * number;
+}
+let number = square (2);/* 
+or console.log(square(2)); */
+console.log(number);
+
+/* greet('Vonn', 'Cruz');
+greet('Imelda','Hoffmann'); */
